e11f0bf0a051d60340067f61ffec32420b45e13a
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-05-30 19:00:57 -0300

    style: makes main image disappear in small screens

--- src/pages/Addresses.jsx
+++ src/pages/Addresses.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import { AddressCard } from "../components/AddressCard";
 import { mockAddresses } from "../data/mockAddresses";
-import Thumbnail from "../assets/thumbnail.jpg";
+import Thumbnail from "../assets/thumbnail-2.jpg";
 
 export function Addresses() {
   return (
     <section className="m-auto flex w-full max-w-screen-xl items-center justify-between p-8">
-      <div className="flex flex-col-reverse gap-4 sm:flex-row">
+      <div className="flex w-full gap-4">
         <div className="flex flex-1 flex-col gap-4">
           <Link to="/add" className="rounded bg-slate-100 p-4">
             + Add Address
@@ -16,7 +16,7 @@ export function Addresses() {
             <AddressCard key={address.id} address={address} />
           ))}
         </div>
-        <figure className="basis-1/2">
+        <figure className="hidden flex-1 sm:block">
           <img src={Thumbnail} alt="" />
         </figure>
       </div>
