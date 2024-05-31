2188c0d637c0ec78f6246deaf59ebdb17358dff4
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-05-30 22:52:12 -0300

    feat: add all fields to form

--- src/pages/AddAddresses.jsx
+++ src/pages/AddAddresses.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import Thumbnail from "../assets/thumbnail-2.jpg";
+import { COUNTRIES } from "../utils/isoCountries";
 
 export function AddAddresses() {
   const [planet, setPlanet] = useState("Earth");
-  const [country, setCountry] = useState("us");
+  const [country, setCountry] = useState("US");
+
+  const countryNames = Object.entries(COUNTRIES);
 
   return (
     <section className="m-auto flex w-full max-w-screen-xl items-center justify-between p-8">
       <div className="flex w-full gap-4">
         <form className="flex flex-1 flex-col gap-4">
-          <h1 className="text-lg font-bold">Add address</h1>
           <label htmlFor="addressName">Address Name</label>
           <input
             type="text"
@@ -36,7 +38,6 @@ export function AddAddresses() {
           />
           <p>Planet</p>
           <div className="flex gap-4 py-2">
-            <label htmlFor="planet-earth">Earth</label>
             <input
               type="radio"
               name="planet"
@@ -45,7 +46,7 @@ export function AddAddresses() {
               checked={planet === "Earth"}
               onChange={(event) => setPlanet(event.target.value)}
             />
-            <label htmlFor="planet-mars">Mars</label>
+            <label htmlFor="planet-earth">Earth</label>
             <input
               type="radio"
               name="planet"
@@ -54,25 +55,47 @@ export function AddAddresses() {
               checked={planet === "Mars"}
               onChange={(event) => setPlanet(event.target.value)}
             />
+            <label htmlFor="planet-mars">Mars</label>
           </div>
 
           {planet === "Earth" && (
             <>
               <label htmlFor="country">Country</label>
               <select
+                required
                 id="country"
                 value={country}
-                onChange={(event) => setCountry(event.target.value)}
-                className="rounded border border-slate-300 p-2 bg-white"
+                name="country"
+                className="rounded border border-slate-300 bg-white p-2 min-h-11"
+                onChange={(event) => {
+                  setCountry(event.target.value);
+                }}
               >
-                <option value="us">United States</option>
-                <option value="ca">Canada</option>
+                {countryNames.map(([id, label]) => {
+                  return (
+                    <option value={id} key={id}>
+                      {label}
+                    </option>
+                  );
+                })}
               </select>
+              <label htmlFor="state">State</label>
+              <input
+                type="text"
+                id="state"
+                className="rounded border border-slate-300 p-2"
+              />
+              <label htmlFor="city">City</label>
+              <input
+                type="text"
+                id="city"
+                className="rounded border border-slate-300 p-2"
+              />
               <label htmlFor="zipCode">Zip Code</label>
               <input
                 type="text"
                 id="zipCode"
-                className="rounded border border-slate-300 p-2 "
+                className="rounded border border-slate-300 p-2"
               />
             </>
           )}
@@ -87,6 +110,8 @@ export function AddAddresses() {
               />
             </>
           )}
+
+          <button type="submit" className="bg-slate-200 rounded p-4">Add Address</button>
         </form>
         <figure className="hidden flex-1 sm:block">
           <img src={Thumbnail} alt="" />
