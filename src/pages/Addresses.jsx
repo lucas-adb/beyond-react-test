68e4373bfa19441640fbb92d37257ba518e65431
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-06-01 10:04:43 -0300

    style: add address button

--- src/pages/Addresses.jsx
+++ src/pages/Addresses.jsx
@@ -17,7 +17,7 @@ export function Addresses() {
       <section className="m-auto flex w-full max-w-screen-xl items-center justify-between p-8">
         <div className="flex w-full gap-4">
           <div className="flex flex-1 flex-col gap-4">
-            <Link to="/add" className="rounded bg-slate-100 p-4">
+            <Link to="/add" className="rounded bg-blue-400 p-4">
               + Add Address
             </Link>
             <h1 className="text-lg font-bold">My Addresses</h1>
@@ -35,7 +35,7 @@ export function Addresses() {
     <section className="m-auto flex w-full max-w-screen-xl items-center justify-between p-8">
       <div className="flex w-full gap-4">
         <div className="flex flex-1 flex-col gap-4">
-          <Link to="/add" className="rounded bg-slate-100 p-4">
+          <Link to="/add" className="rounded-lg border-2 border-green-500 p-4 font-bold text-green-500 hover:bg-green-500 hover:text-white">
             + Add Address
           </Link>
           <h1 className="text-lg font-bold">My Addresses</h1>
