d26ee0286d792b5616fcfc9d01fbddaeba9e15a5
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-06-01 10:21:59 -0300

    refactor: adds loading state and addresses.length < 1 component to Addresses

--- src/pages/Addresses.jsx
+++ src/pages/Addresses.jsx
@@ -6,42 +6,42 @@ import { getAddresses } from "../utils/firebaseFunctions";
 
 export function Addresses() {
   const [addresses, setAddresses] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = getAddresses(setAddresses);
+    const unsubscribe = getAddresses((addresses) => {
+      setAddresses(addresses);
+      setLoading(false);
+    });
     return () => unsubscribe();
   }, []);
-
-  if (addresses.length < 1) {
-    return (
-      <section className="m-auto flex w-full max-w-screen-xl items-center justify-between p-8">
-        <div className="flex w-full gap-4">
-          <div className="flex flex-1 flex-col gap-4">
-            <Link to="/add" className="rounded bg-blue-400 p-4">
-              + Add Address
-            </Link>
-            <h1 className="text-lg font-bold">My Addresses</h1>
-            <p className="animate-pulse">Loading. Please wait...</p>
-          </div>
-          <figure className="hidden flex-1 sm:block">
-            <img src={Thumbnail} alt="" />
-          </figure>
-        </div>
-      </section>
-    );
-  }
 
   return (
     <section className="m-auto flex w-full max-w-screen-xl items-center justify-between p-8">
       <div className="flex w-full gap-4">
         <div className="flex flex-1 flex-col gap-4">
-          <Link to="/add" className="rounded-lg border-2 border-green-500 p-4 font-bold text-green-500 hover:bg-green-500 hover:text-white">
+          <Link
+            to="/add"
+            className="rounded-lg border-2 border-green-500 p-4 font-bold text-green-500 hover:bg-green-500 hover:text-white"
+          >
             + Add Address
           </Link>
           <h1 className="text-lg font-bold">My Addresses</h1>
-          {addresses.map((address) => (
-            <AddressCard key={address.id} address={address} />
-          ))}
+
+          {loading && (
+            <p className="animate-pulse text-lg font-bold">
+              Loading. Please wait...
+            </p>
+          )}
+
+          {addresses.length < 1 ? (
+            <p className="text-lg font-bold">You have no saved addresses</p>
+          ) : (
+            addresses.map((address) => (
+              <AddressCard key={address.id} address={address} />
+            ))
+          )}
+
         </div>
         <figure className="hidden flex-1 sm:block">
           <img src={Thumbnail} alt="" />
