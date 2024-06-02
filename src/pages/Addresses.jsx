39efda8e009bb055ccd959eddcdbe2d8db8e07e9
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-06-02 00:19:03 -0300

    refactor: remove unused images

--- src/pages/Addresses.jsx
+++ src/pages/Addresses.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { AddressCard } from "../components/AddressCard";
-import Thumbnail from "../assets/rh_03.jpg";
+import Thumbnail from "../assets/rh_02.jpg";
 import { useEffect, useState } from "react";
 import { getAddresses } from "../utils/firebaseFunctions";
 
